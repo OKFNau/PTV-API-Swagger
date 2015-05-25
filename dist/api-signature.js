7ba959a41dfe5147fcc33ded1161825bf8a6a5ac
Steve Bennett <stevage@gmail.com>
2015-05-25 15:55:03 +1000

    OMG you can test the API in JavaScript now!

--- dist/api-signature.js
+++ dist/api-signature.js
@@ -2,12 +2,50 @@ function parameterChange(e) {
 
 }
 
+function getURL(sandbox) {
+  var path = $(sandbox).parent().parent().find(".path").text().trim();
+  var moreargs="?"
+  if (path.match(/healthcheck/)) {
+    path += "?timestamp={timestamp}";
+    moreargs="&";
+  }
+  if (path.match(/stopping-pattern/)) {
+    path += "?for_utc={for_utc}";
+    moreargs="&";
+  }
+  path += moreargs + "devid={devid}" 
+  var params=path.match(/{([^{}])*}/g);
+  for (i=0; i < params.length; i++) {
+    var param=params[i].replace(/[{}]/g,"");
+    var pval = $(sandbox).find("input[name='" + param + "']").val();
+    path = path.replace(params[i], encodeURIComponent(pval));
+  }
+  console.log(path);
+  return path;
+
+  // elements = $($(".sandbox")[0]).find("input[name='timestamp']")
+}
+
 function setSignatureHooks() {
+  $("#input_apiKey").hide();
   $(".sandbox").each(function(i,sandbox) {
     var sigbox = $(sandbox).find("input[name='signature']");
+    var devid = $(sandbox).find("input[name='devid']").val();
     $(sandbox).find("input[type='text'][name!='signature']").each(function(i, textbox) {
       $(textbox).change (function(e) {
-        $(sigbox).val($(textbox).val());
+        var timestamp = new Date().toISOString().replace(/\.\d*Z$/,"");
+        $("input[name='timestamp']").val(timestamp);
+
+
+        var devkey = $("#input_devkey").val(); 
+        var call = getURL(sandbox);
+        console.log("Get HMAC of: " + call);
+        var shaObj = new jsSHA(call, "TEXT");
+        var hmac = shaObj.getHMAC(devkey, "TEXT", "SHA-1", "HEX");
+        console.log("Result: " + hmac);
+        $(sigbox).val(hmac.toUpperCase());
+
+        // $($(".sandbox")[0]).parent().parent().find(".path").text().trim()
       });
     });
   });
