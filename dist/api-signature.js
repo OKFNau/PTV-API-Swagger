a22c76ed5d9307f6e4ca91a0a45d949e8a9d4ef8
Steve Bennett <stevage@gmail.com>
2015-05-25 22:30:50 +1000

    Use cookies to store devid, devkey.

--- dist/api-signature.js
+++ dist/api-signature.js
@@ -26,6 +26,55 @@ function getURL(sandbox) {
   // elements = $($(".sandbox")[0]).find("input[name='timestamp']")
 }
 
+function doCookies() {
+  var m = document.cookie.match("ptv_devid=([^; ]+)");
+  if (m) {
+    $("#input_devid").val(m[1]);
+    $("input[name='devid']").each(function(i, e) {
+      $(e).val(m[1]); 
+    });
+  }
+  m = document.cookie.match("ptv_devkey=([^; ]+)");
+  if (m) {
+    $("#input_devkey").val(m[1]);
+  }
+}
+
+function devIdChange(ev) {
+  var devid = $(this).val();
+  $("input[name='devid']").each(function(i, e) {
+    $(e).val(devid); 
+  });
+
+  setCookie("ptv_devid", devid, 7);
+}
+
+function devKeyChange(ev) {
+  var devkey = $(this).val();
+
+  setCookie("ptv_devkey", devkey, 7);
+}
+function setCookie(cname, cvalue, exdays) {
+  // from http://www.w3schools.com/js/js_cookies.asp dear god
+    var d = new Date();
+    d.setTime(d.getTime() + (exdays*24*60*60*1000));
+    var expires = "expires="+d.toUTCString();
+    document.cookie = cname + "=" + cvalue + "; " + expires;
+}
+
+var paramBoxChange = function(sandbox, sigbox) {
+  return function(e) {
+    var devkey = $("#input_devkey").val(); 
+    var call = getURL(sandbox);
+    console.log("Get HMAC of: " + call);
+    var shaObj = new jsSHA(call, "TEXT");
+    var hmac = shaObj.getHMAC(devkey, "TEXT", "SHA-1", "HEX");
+    console.log("Result: " + hmac);
+    $(sigbox).val(hmac.toUpperCase());
+  }
+}
+
+
 function setSignatureHooks() {
   $("#api_info").append(      
     '<div class="input devkeybox">' +
@@ -38,36 +87,24 @@ function setSignatureHooks() {
 
     );
 
-  $("#input_apiKey").hide();
-  $("#header").hide();
-  // set all devids once you change one.
-  $("input[name='devid'],#input_devid").change(function(ev) {
-    var devid = $(this).val();
-    $("input[name='devid']").each(function(i, e) {
-      $(e).val(devid); 
-    });
-  });
-
   $(".sandbox").each(function(i,sandbox) {
     var sigbox = $(sandbox).find("input[name='signature']");
     var devid = $(sandbox).find("input[name='devid']").val();
     $(sandbox).find("input[type='text'][name!='signature']").each(function(i, textbox) {
-      $(textbox).change (function(e) {
-        var timestamp = new Date().toISOString().replace(/\.\d*Z$/,"");
-        $("input[name='timestamp']").val(timestamp);
-
-
-        var devkey = $("#input_devkey").val(); 
-        var call = getURL(sandbox);
-        console.log("Get HMAC of: " + call);
-        var shaObj = new jsSHA(call, "TEXT");
-        var hmac = shaObj.getHMAC(devkey, "TEXT", "SHA-1", "HEX");
-        console.log("Result: " + hmac);
-        $(sigbox).val(hmac.toUpperCase());
-
-        // $($(".sandbox")[0]).parent().parent().find(".path").text().trim()
-      });
+      $(textbox).change (paramBoxChange(sandbox, sigbox));
     });
   });
+
+  doCookies();
+
+  var timestamp = new Date().toISOString().replace(/\.\d*Z$/,"");
+  $("input[name='timestamp']").val(timestamp);
+
+  $("#input_apiKey").hide();
+  $("#header").hide();
+  // set all devids once you change one.
+  $("input[name='devid'],#input_devid").change(devIdChange);
+  $("#input_devkey").change(devKeyChange);
+
 
 }
